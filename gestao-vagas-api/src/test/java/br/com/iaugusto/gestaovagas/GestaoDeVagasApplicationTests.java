c8d18faa3d0d3b001c523cf71191f1916e3fc966
Igor Augusto <igoraugustobz@gmail.com>
2023-11-26 14:54:28 -0300

    feat: adição do código inicial

---
+++ gestao-vagas-api/src/test/java/br/com/iaugusto/gestaovagas/GestaoDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iaugusto.gestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
